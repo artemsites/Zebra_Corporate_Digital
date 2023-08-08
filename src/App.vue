a00c92420c09d042b1db66e8805720e98218f68a
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2023-08-08 20:56:22 +0400

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+  
+</style>
