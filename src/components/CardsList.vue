2e881125e98408ef9ea079a8fcee489907002e50
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2023-08-09 15:27:11 +0400

    сделал вывод карточек и кнопку загрузить ещё

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,25 +1,26 @@
 <template>
   <section class="cards-list container">
+    <div class="cards-list__box">
+      <article class="card" :class="{ '_no-img': !card.image }" v-for="card in cards" :key="card.code" :id="card.code">
+        <img v-if="card.image" class="card__img" :src="card.image" :alt="card.name">
+        <div class="card__box">
+          <time datetime="2018-07-07" class="card__date">
+            <span class="card__date-d">{{ moment.unix(card.date).format('DD') }}</span>
+            <span class="card__date-my">
+              {{ moment.unix(card.date).format('MMMM') }}
+              <br>
+              {{ moment.unix(card.date).format('YYYY') }}
+            </span>
+          </time>
 
-    <article class="card" v-for="card in cards" :key="card.code" :id="card.code">
-      <img v-if="card.image" class="card__img" :src="card.image" :alt="card.name">
-      <div class="card__box">
-        <time datetime="2018-07-07" class="card__date">
-          <span class="card__date-d">{{ moment.unix(card.date).format('DD') }}</span>
-          <span class="card__date-my">
-            {{ moment.unix(card.date).format('MMMM') }}
-            <br>
-            {{ moment.unix(card.date).format('YYYY')}}
-          </span>
-        </time>
-
-        <h2 class="card__title">{{ card.name }}</h2>
-        <p class="card__text">{{ card.previewText }}</p>
+          <h2 class="card__title" v-html="card.name"></h2>
+          <p class="card__text" v-html="card.previewText"></p>
+        </div>
         <RouterLink :to="`#${card.link}`" class="card__btn">Новости</RouterLink>
-      </div>
-    </article>
+      </article>
+    </div>
 
-
+    <button v-if="showBtnLoadNextPage" @click="showNextPage" class="cards-list__show-more">Загрузить ещё</button>
   </section>
 </template>
 
@@ -30,21 +31,68 @@ import moment from 'moment'
 
 
 
+const API = 'https://flems.github.io/test/api/news'
 let cards = ref([])
+let page = ref({ 'cur': null, 'total': null })
+let showBtnLoadNextPage = ref(false)
+
+
 
 /**
  * При первой загрузке страницы 
  *   необходимо отправлять AJAX запрос на обработчик ([http://flems.github.io/test/api/news](http://flems.github.io/test/api/news/2/)/) и получать данные в виде JSON. 
  * 
- * Эти данные необходимо обработать и вывести новости на страницу. Проверяем есть ли возможность загрузить следующую страницу. Если есть, то показываем кнопку "Загрузить еще".
+ * Эти данные необходимо обработать и вывести новости на страницу. 
  */
-fetch('https://flems.github.io/test/api/news')
-  .then(response => response.json())
-  .then(data => {
-    console.log('data')
-    console.log(data)
-    cards.value = data.items
-  });
+showPage(1)
+  .then(() => {
+    /**
+     * Проверяем есть ли возможность загрузить следующую страницу. Если есть, то показываем кнопку "Загрузить еще".
+     */
+    ifNextPageExistShowBtnLoadElseHide()
+  })
 
 
-</script> 
+
+async function showPage(num) {
+  if (num === 1) num = ''
+  await fetch(`${API}/${num}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log('data')
+      console.log(data)
+      cards.value.push(...data.items)
+      // cards.value = data.items
+      page.value.cur = data.nav.current
+      page.value.total = data.nav.total
+    })
+}
+
+
+
+function ifNextPageExistShowBtnLoadElseHide() {
+  if (page.value.cur < page.value.total) {
+    fetch(`${API}/${page.value.cur + 1}`)
+      .then(response => response.json())
+      .then(nextPageData => {
+        if (nextPageData.items.length > 0) {
+          showBtnLoadNextPage.value = true
+        }
+      });
+  }
+  else {
+    showBtnLoadNextPage.value = false
+  }
+}
+
+
+
+function showNextPage() {
+  if (page.value.cur < page.value.total) {
+    showPage(page.value.cur + 1)
+      .then(() => {
+        ifNextPageExistShowBtnLoadElseHide()
+      })
+  }
+}
+</script>
