989bf9430d6bf7fbc4216a40f19e4529b967f43d
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2023-08-09 15:27:17 +0400

    сверстал подвал

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <script setup>
   import { /* RouterLink,  */RouterView } from 'vue-router'
   import IconLogo from '@/components/icons/IconLogo.vue'
+  import Copyright from '@/components/Copyright.vue'
 </script>
 
 <template>
   <header class="header">
     <div class="container">
-      <IconLogo />
+      <IconLogo class="header__logo" />
     </div>
   </header>
 
+
+
   <RouterView />
+
+
+
+  <header class="footer">
+    <div class="container">
+      <IconLogo class="footer__logo" />
+      <Copyright class="footer__copyright" />
+    </div>
+  </header>
 </template>
 
 <style lang="scss">
---
+++ src/components/Copyright.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="copyright">
+    © alpha, 2023
+  </div>
+</template>
