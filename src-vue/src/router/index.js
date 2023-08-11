0d5aa32d6e3d485761e30b73b180f56b5aae4d95
Artem Kuznecov <artem.kuznecov.samara@yandex.ru>
2023-08-11 16:43:46 +0400

    testRoot

--- src-vue/src/router/index.js
+++ src-vue/src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
+let testRoot = '/Zebra_Corporate_Digital/'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/(/ || test/ || Zebra_Corporate_Digital/)',
+      path: testRoot,
       name: 'home',
       component: HomeView
     },
